056565a9c7dd04ea6cfd6babb8d20ccf4208f5d2
Thamilezai Ananthakumar <156745488+ThamilezaiAnanthakumar@users.noreply.github.com>
2025-03-24 01:02:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ def calibrate():
             return d * (ptt ** -e) + f
 
         try:
-            params_sbp, _ = opt.curve_fit(sbp_model, ptt_values_c, sbp_values_c, p0=[1, 1, 100], maxfev=10000)
+            params_sbp, _ = opt.curve_fit(sbp_model, ptt_values_c, sbp_values_c, p0=[1, 0, 0], maxfev=10000)
            #params_sbp, _ = opt.curve_fit(sbp_model, ptt_values_c, sbp_values_c, p0=[1, 1, 100], maxfev=10000, bounds=([0, 0, 0], [np.inf, np.inf, np.inf]))
             st.write("Fitting successful:", params_sbp)
         except Exception as e:
